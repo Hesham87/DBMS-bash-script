6938e7b575b0a9d551ef9277e1f637b7ad32c9e1
Hesham87ls <heshammedhat87@gmail.com>
2024-11-15 16:37:02 +0200

    adding comments

--- DBMS.sh
+++ DBMS.sh
@@ -34,16 +34,16 @@ createTB(){
                     print $i >> ("tempfile.txt")                                                    # [1-9][0-9]{0,2}: Matches a number from 1 to 999 (e.g., 1, 50, 999).
                                                                                                     # \}$: Ends with a closing }.
                 }                                                                                   # -E, --extended-regexp     PATTERNS are extended regular expressions Provides more advanced syntax compared to basic regular expressions (BRE).
-                 # Matches "Int(number)" where number is between 1 and 99999999                                                 # You don't need to escape certain metacharacters (e.g., +, |, ()).
+                 # Matches "Int(number)" where number is between 1 and 99999999                                                 # You dont need to escape certain metacharacters (e.g., +, |, ()).
                 else if (system("echo " $i " | grep -qE \"^Int\{[1-9][0-9]{0,7}\}$\"") == 0) {      # -G, --basic-regexp        PATTERNS are basic regular expressions (Requires escaping for metacharacters like +, |, and () to be treated as special regex constructs.)
                     print $i >> ("tempfile.txt")                                                    # -P, --perl-regexp         PATTERNS are Perl regular expressions (supports advanced constructs like lookaheads, lookbehinds, and non-capturing groups.)
                 }                                                                                                               # Examples:
-                else if ($i == "Date") {                                                                                        # echo "apple123orange" | grep -P "(?<=apple)\d+"  # Matches digits after 'apple' (lookbehind)
+                else if ($i == "Date") {                                                                                        # echo "apple123orange" | grep -P "(?<=apple)\d+"  # Matches digits after "apple" (lookbehind)
                     print $i >> ("tempfile.txt")                                                                                # echo "1234" | grep -P "\d+"                     # Matches one or more digits (PCRE '\d')
                 }                                                                                   # -e, --regexp=PATTERNS     use PATTERNS for matching  (when combining multiple patterns in a single grep command.) 
                 else {                                                                                                          # Examples:
-                    print "Invalid input: Supported types are Primary_key, Int{1-99999999}, Char{1-999}, Date"                  # echo "apple orange banana" | grep -e "apple" -e "banana"  # Matches 'apple' or 'banana'
-                    exit 1                                                                                                      # echo "fruit" | grep -e "fruit"                           # Matches 'fruit'
+                    print "Invalid input: Supported types are Primary_key, Int{1-99999999}, Char{1-999}, Date"                  # echo "apple orange banana" | grep -e "apple" -e "banana"  # Matches "apple" or "banana"
+                    exit 1                                                                                                      # echo "fruit" | grep -e "fruit"                           # Matches "fruit"
                 }                                                                                   # -q, --quiet, --silent     suppress all normal output
             }      
              else{
